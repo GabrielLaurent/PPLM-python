0dc5c394111a0d99701bd12787dfbf8cca6a0d2e
GabrielLaurent <gabriellaurent.embellish948@slmail.me>
2023-01-04 09:46:26 +0800

    Implement: Implement Quantitative Evaluation Metrics\ (#6)

--- experiments/run_experiment.py
+++ experiments/run_experiment.py
@@ -1,26 +1,76 @@
-import sys
-sys.path.append('.')
-import argparse
-from src.pplm.pplm import PPLM
-from src.models.language_model import LanguageModel
-from src.models.discriminator import Discriminator
-from src.utils.evaluation import evaluate_text
+import torch
+import numpy as np
+from src.models.language_model import LanguageModel  # Assuming you have a LanguageModel class
+from src.models.discriminator import Discriminator  # Assuming you have a Discriminator class
+from src.pplm.pplm import PPLM  # Assuming you have a PPLM class
+from src.utils.generation import generate_text
+from src.utils.evaluation import calculate_perplexity, calculate_discriminator_score
 
-def main():
-    parser = argparse.ArgumentParser(description='Run PPLM experiment.')
-    parser.add_argument('--prompt', type=str, default='The weather is', help='Initial prompt.')
-    parser.add_argument('--attribute', type=str, default='positive', help='Attribute to control.')
-    parser.add_argument('--length', type=int, default=50, help='Length of generated text.')
-    args = parser.parse_args()
 
-    language_model = LanguageModel()
-    discriminator = Discriminator('roberta-base-sentiment')
-    pplm = PPLM(language_model, discriminator)
-    generated_text = pplm.generate_text(args.prompt, args.attribute, args.length)
-    evaluation_result = evaluate_text(generated_text, args.attribute)
+def run_experiment(language_model_config, discriminator_config, pplm_config, experiment_config):
+    # Initialize language model, discriminator, and PPLM
+    language_model = LanguageModel(**language_model_config)
+    discriminator = Discriminator(**discriminator_config)
+    pplm = PPLM(language_model, discriminator, **pplm_config)
 
-    print(f"Generated Text: {generated_text}")
-    print(f"Evaluation Result: {evaluation_result}")
+    # Load models if specified
+    if experiment_config.get('load_language_model', None):
+        language_model.load_state_dict(torch.load(experiment_config['load_language_model']))
+    if experiment_config.get('load_discriminator', None):
+        discriminator.load_state_dict(torch.load(experiment_config['load_discriminator']))
+
+    # Define generation parameters (example)
+    prompt = experiment_config.get('prompt', "The cat sat on the")
+    length = experiment_config.get('length', 50)
+    num_samples = experiment_config.get('num_samples', 10)
+
+    # Generation loop
+    generated_texts = []
+    for i in range(num_samples):
+        generated_text = generate_text(pplm, prompt, length)
+        generated_texts.append(generated_text)
+
+        # Quantitative Evaluation
+        # Assume the generate_text function returns the tokenized sequence in addition to the text
+        # and that the actual ids are accessible at index 1 (adjust if needed).
+        # Example: generated_text = (text_string, token_ids)
+        if isinstance(generated_text, tuple):
+          text_string, token_ids = generated_text
+          perplexity = calculate_perplexity(language_model.forward(torch.tensor(token_ids).unsqueeze(0)).logits.squeeze(), torch.tensor(token_ids))
+          discriminator_score = calculate_discriminator_score(discriminator, torch.tensor(token_ids).unsqueeze(0))
+
+          print(f"Sample {i+1}:\nText: {text_string}\nPerplexity: {perplexity}\nDiscriminator Score: {discriminator_score}\n")
+        else:
+          print(f'Text generation function needs to return a tuple containing the text and token ids')
+          print(f"Sample {i+1}:\nText: {generated_text}\nPerplexity: N/A\nDiscriminator Score: N/A\n")
+
+    # Save generated texts (optional)
+    # ...
 
 if __name__ == '__main__':
-    main()
+    # Example configurations (replace with your actual configurations)
+    language_model_config = {
+        'vocab_size': 50257,  # Example value, replace with your actual vocab size
+        'n_embd': 768,        # Example value
+        'n_head': 12,         # Example value
+        'n_layer': 12        # Example value
+    }
+    discriminator_config = {
+        'input_size': 768,  # Example value, assuming the language model's embedding size for simplicity
+        'hidden_size': 256,  # Example value
+        'num_layers': 2      # Example value
+    }
+    pplm_config = {
+        'step_size': 0.02,
+        'kl_scale': 0.01,
+        'gm_scale': 0.95
+    }
+    experiment_config = {
+        'prompt': "The cat",
+        'length': 20,
+        'num_samples': 2, # Reduced for demonstration
+        'load_language_model': None,
+        'load_discriminator': None,
+    }
+
+    run_experiment(language_model_config, discriminator_config, pplm_config, experiment_config)
--- src/utils/evaluation.py
+++ src/utils/evaluation.py
@@ -1,5 +1,50 @@
-def evaluate_text(generated_text, attribute):
-    # Placeholder for evaluation logic
-    # Should return evaluation metrics
-    print(f"Evaluating text: {generated_text} for attribute: {attribute}")
-    return {"score": 0.8}  # Dummy score for testing
+import torch
+import torch.nn.functional as F
+
+
+def calculate_perplexity(logits, labels):
+    """Calculates perplexity given logits and labels.
+
+    Args:
+        logits (torch.Tensor): Logits from the language model.
+        labels (torch.Tensor): Ground truth labels.
+
+    Returns:
+        float: Perplexity score.
+    """
+    shift_logits = logits[:-1, ...].contiguous().view(-1, logits.size(-1))
+    shift_labels = labels[1:].contiguous().view(-1)
+    loss = F.cross_entropy(shift_logits, shift_labels)
+    perplexity = torch.exp(loss)
+    return perplexity.item()
+
+
+def calculate_discriminator_score(discriminator, text):
+    """Calculates the discriminator score for a given text.
+
+    Args:
+        discriminator: The discriminator model.
+        text (str): The generated text.
+
+    Returns:
+        float: The discriminator score.
+    """
+    # Tokenize the text (implementation depends on your tokenizer)
+    # This is a placeholder - replace with your actual tokenization process
+    # Example using a hypothetical tokenizer:
+    # tokenized_text = tokenizer.encode(text, return_tensors='pt')
+    # Assuming the discriminator takes token ids as input
+    if isinstance(text, str):
+        return 0 # default value if text is not processed. In the experiment section, this value will be used to determine if an actual sequence has been processed
+
+    with torch.no_grad():
+        # Use try-except to handle cases where the text is too short or causes errors
+        try:
+            discriminator_output = discriminator(text)
+            # Assuming the discriminator returns a score representing the probability
+            # that the text belongs to the target domain
+            score = torch.sigmoid(discriminator_output).item()
+            return score
+        except Exception as e:
+            print(f"Error calculating discriminator score: {e}")
+            return 0.0 # or some other default value
